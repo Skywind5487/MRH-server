be891092442faed7f9f0a28cf7fe02596a5512ba
Skywind5487 <skywind5487@gmail.com>
2022-08-27 12:24:48 +0800

    new-sever-version

--- backup.bat
+++ backup.bat
@@ -1,9 +1,6 @@
 @echo off
-CD ".\world\datapacks"
 call "datapack_update.bat"
-CD "..\..\..\"
-pause
 git add --all
 git commit -m "new-sever-version"
 git push 
-pause
+
--- world/datapacks/datapack_update.bat
+++
@@ -1,2 +0,0 @@
-cd "./MRH_datapack"
-git pull
